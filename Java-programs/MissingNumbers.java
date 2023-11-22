a7d233d87156d5b097d61f1a93d5c6b6c78c5d6f
Ayeshasiddiqua-28 <150899720+Ayeshasiddiqua-28@users.noreply.github.com>
2023-11-22 16:27:10 +0530

    Update MissingNumbers.java

--- Java-programs/MissingNumbers.java
+++ Java-programs/MissingNumbers.java
@@ -1,1 +1,30 @@
+package Javabasics;
+import java.util.Arrays;
+
+public class MissingNumbers {
+
+    static int findMissingNumber(int[] arr) {
+        int n = arr.length + 1;
+        int totalSum = n * (n + 1) / 2;
+        int currentSum = 0;
+
+        for (int num : arr) {
+            currentSum += num;
+        }
+
+        return totalSum - currentSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3,  5, 6, 7, 8, 9};
+
+       // Print the array
+        System.out.println("The given array is: " + Arrays.toString(arr));
+
+       // Find the missing number
+        int missingNumber = findMissingNumber(arr);
+
+        System.out.println("The missing number is: " + missingNumber);
+    }
+}
 
